a8fd6cda527da1d21f3d240d677601e2ac5989a1
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-08-09 16:36:02 -0300

    rest - post put delete addresses

--- app/Http/Controllers/AddressesController.php
+++ app/Http/Controllers/AddressesController.php
@@ -33,46 +33,72 @@ class AddressesController extends Controller
         return son_response()->make($result);
     }
 
-    public function store(Request $request)
+    public function store(Request $request, $clientId)
     {
+        if (!($client = Client::find($clientId))) {
+            throw new ModelNotFoundException("Client requisitado não existe");
+        }
+
         $this->validate($request, [
-            'name' => 'required',
-            'email' => 'required',
-            'phone' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'zipcode' => 'required'
         ]);
 
-        $client = Client::create($request->all());
+        $address = $client->addresses()->create($request->all());
 
-        return son_response()->make($client, 201);
+        return son_response()->make($address,201);
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $id, $clientId)
     {
-        if (!($client = Client::find($id))) {
+        if (!(Client::find($clientId))) {
             throw new ModelNotFoundException("Client requisitado não existe");
-        } else {
-            $this->validate($request, [
-                'name' => 'required',
-                'email' => 'required',
-                'phone' => 'required',
-            ]);
-
-            $client->fill($request->all());
-
-            $client->save();
-
-            return son_response()->make($client, 200);
         }
+
+        if (!(Address::find($id))) {
+            throw new ModelNotFoundException("Endereço requisitado não existe");
+        }
+
+        $this->validate($request, [
+            'address' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'zipcode' => 'required'
+        ]);
+
+        $address = Address::where('client_id', $clientId)->where('id', $id)->get()->first();
+
+        if (!$address) {
+            throw new ModelNotFoundException("Endereço requisitado não existe");
+        }
+
+        $address->fill($request->all());
+
+        $address->save();
+
+        return son_response()->make($address, 200);
     }
 
-    public function destroy($id)
+    public function destroy($id, $clientId)
     {
-        if (!($client = Client::find($id))) {
+        if (!(Client::find($clientId))) {
             throw new ModelNotFoundException("Client requisitado não existe");
-        } else {
-            $client->delete();
-
-            return son_response()->make("", 204);
         }
+
+        if (!(Address::find($id))) {
+            throw new ModelNotFoundException("Endereço requisitado não existe");
+        }
+
+        $address = Address::where('client_id', $clientId)->where('id', $id)->get()->first();
+
+        if (!$address) {
+            throw new ModelNotFoundException("Endereço requisitado não existe");
+        }
+
+        $address->delete();
+
+        return son_response()->make("", 204);
     }
 }
